b2618111708434fb68361649d890e43fbfeac9b3
eastten <account.kk@gamil.com>
2021-05-26 16:17:08 +0900

    ヒーロータイトルにアニメーションを設定

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,5 +5,6 @@ module.exports = {
     "font-family-no-missing-generic-family-keyword": null,
     "at-rule-no-unknown": null, //scssで使える @include などにエラーがでないようにする
     "declaration-colon-newline-after": null, //複数行の時のコロン後に改行がなくてもよい
+    "no-descending-specificity": null,
   },
 };
